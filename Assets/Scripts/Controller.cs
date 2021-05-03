64479ed9539cbbf4495599ecec10cd4f03dbd9f4
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-05-03 19:00:46 -0400

    write Controller script

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	//returns tag of the target being targeted by the invoker
+	protected string GetTargetTag(string tag)
+	{
+		switch (tag)
+		{
+			case "Pants":
+				return "Anvil";
+			case "Fire":
+				return "Pants";
+			case "Anvil":
+				return "Fire";
+			default:
+				Debug.Log("GetTargetTag(): Tag not found");
+				return null;
+		}
+	}
+	
+	//returns tag of the seeker seeking the invoker
+	protected string GetSeekerTag(string tag)
+	{
+		switch (tag)
+		{
+			case "Pants":
+				return "Fire";
+			case "Fire":
+				return "Anvil";
+			case "Anvil":
+				return "Pants";
+			default:
+				Debug.Log("GetSeekerTag(): Tag not found");
+				return null;
+		}
+	}
+}
