0d6d2e7e8c40edccac36da47bcd43e4d99f7bc9c
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-05-03 18:53:59 -0400

    write PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private GridOverlayBehavior overlay;
+	[SerializeField] private GameObject turnIndicator;
+	[SerializeField] private AudioSource audioManager;
+	private GameObject target, seeker;
+	string tag;
+
+	private void Awake()
+	{
+		tag = gameObject.tag;
+		target = GameObject.FindGameObjectWithTag(GetTargetTag(tag));
+		seeker = GameObject.FindGameObjectWithTag(GetSeekerTag(tag));
+	}
+
+
+	public IEnumerator PlayerTurn()
+	{
+		overlay.showPlayerMovementArea(transform.position, gameObject);
+		yield return StartCoroutine(overlay.waitForClick(playerPos =>
+		{
+			transform.position = playerPos;
+			turnIndicator.transform.position = playerPos + Vector3.up;
+		}));
+		yield return new WaitForSeconds(0.5f);
+		if (gameObject != null && target != null && transform.position.x == target.transform.position.x && transform.position.y == target.transform.position.y) //if crush
+		{
+			target.transform.position = new Vector3(0, 20, 0);
+			target.GetComponent<Entity>().Set(false);
+			audioManager.Play();
+		}
+		if (transform.position.x == seeker.transform.position.x && transform.position.y == seeker.transform.position.y) //if is crushed
+		{
+			transform.position = new Vector3(0, 20, 0);
+			gameObject.GetComponent<Entity>().Set(false);
+			seeker.transform.position = new Vector3(0, 20, 0);
+			seeker.GetComponent<Entity>().Set(false);
+			audioManager.Play();
+
+		}
+	}
+
+	string GetTargetTag(string tag)
+	{
+		return "";
+	}
+
+	string GetSeekerTag(string tag)
+	{
+		return "";
+	}
+}
