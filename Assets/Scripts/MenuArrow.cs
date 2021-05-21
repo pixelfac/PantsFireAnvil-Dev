eb669834e532593747fe8a99d530b95dd87fcbc6
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-05-20 22:47:41 -0400

    added TitleScreen moving arrows

---
+++ Assets/Scripts/MenuArrow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MenuArrow : MonoBehaviour
+{
+	public void MoveToStartButton()
+	{
+		transform.position = new Vector3(transform.position.x, 2.95f, transform.position.z);
+	}
+
+	public void MoveToMPButton()
+	{
+		transform.position = new Vector3(transform.position.x, 1.2f, transform.position.z);
+	}
+
+	public void MoveToHowToPlayButton()
+	{
+		transform.position = new Vector3(transform.position.x, -0.7f, transform.position.z);
+	}
+
+	public void MoveToExitButton()
+	{
+		transform.position = new Vector3(transform.position.x, -2.6f, transform.position.z);
+	}
+
+}
