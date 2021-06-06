d5d347400e6a56421afc820098735d674119c096
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-06-05 23:21:05 -0400

    write Highscore_Listing.cs

---
+++ Assets/Scripts/Highscore_Listing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore_Listing : MonoBehaviour
+{
+	[SerializeField] string[] orderedLevelNames;
+	[SerializeField] Text[] scoreTextReferences;
+	[SerializeField] int listingCharWidth;
+
+	private void Awake()
+	{
+		for (int i=0; i < orderedLevelNames.Length; i++)
+		{
+			string scoreAsString = HighScoreManager.GetScore(orderedLevelNames[i]).ToString();
+			scoreTextReferences[i].text = scoreAsString.PadLeft(listingCharWidth, '.');
+		}
+	}
+}
