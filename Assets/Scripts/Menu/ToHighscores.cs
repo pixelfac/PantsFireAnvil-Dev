affb5774bf1c131776bc887ab380d7df97a3d80b
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-06-06 20:12:18 -0400

    write ToHighscore.cs

---
+++ Assets/Scripts/Menu/ToHighscores.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToHighscores : MonoBehaviour
+{
+    public void main()
+	{
+		SceneManager.LoadScene("Highscores");
+	}
+}
