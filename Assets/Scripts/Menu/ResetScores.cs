f2ae27547bdd257796104a609a33528cea412f85
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-06-11 23:39:43 -0400

    write ResetScores script
    it just references the ResetScores() static func from HighscoreManager to be used in a button

---
+++ Assets/Scripts/Menu/ResetScores.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScores : MonoBehaviour
+{
+    public void main()
+	{
+		HighScoreManager.ResetScores();
+	}
+}
