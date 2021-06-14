53927535858f42473a6dd1fac404ce5c472e20c8
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-06-13 23:18:31 -0400

    add static enable function to CursorTracker

--- Assets/Scripts/CursorTracker.cs
+++ Assets/Scripts/CursorTracker.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class CursorTracker : MonoBehaviour
 {
+	static bool enabled;
+
+	private void Awake()
+	{
+		enabled = true;
+	}
 	// Update is called once per frame
 	void Update()
 	{
+		if (!enabled) return;
+
 		//moves CursorTracker object to follow the cursor while staying locked to the grid
 		Vector3 mousePos = new Vector3((int)Input.mousePosition.x, (int)Input.mousePosition.y, 1f);
 		mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 		Vector3 RoundedmousePos = new Vector3(Mathf.RoundToInt(mousePos.x), Mathf.RoundToInt(mousePos.y), 1f);
 		transform.position = RoundedmousePos;
+	}
+
+	public static void SetCursorTracker(bool status)
+	{
+		enabled = status;
 	}
 }
