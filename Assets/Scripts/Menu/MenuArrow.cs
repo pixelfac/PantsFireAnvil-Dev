7b890c873d27bfcb89c3fd2732829994e248d273
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-06-15 23:52:12 -0400

    update menu arrows to match x pos as well as y

--- Assets/Scripts/Menu/MenuArrow.cs
+++ Assets/Scripts/Menu/MenuArrow.cs
@@ -2,19 +2,28 @@
 
 public class MenuArrow : MonoBehaviour
 {
+
+	[SerializeField] Transform arrowLeft, arrowRight;
+
 	public void MoveToStartButton()
 	{
 		transform.position = new Vector3(transform.position.x, 1.1f, transform.position.z);
+		arrowLeft.position = new Vector3(-5, arrowLeft.position.y, arrowLeft.position.z);
+		arrowRight.position = new Vector3(5, arrowRight.position.y, arrowRight.position.z);
 	}
 
 	public void MoveToHowToPlayButton()
 	{
 		transform.position = new Vector3(transform.position.x, -0.7f, transform.position.z);
+		arrowLeft.position = new Vector3(-4, arrowLeft.position.y, arrowLeft.position.z);
+		arrowRight.position = new Vector3(4, arrowRight.position.y, arrowRight.position.z);
 	}
 
 	public void MoveToExitButton()
 	{
 		transform.position = new Vector3(transform.position.x, -2.6f, transform.position.z);
+		arrowLeft.position = new Vector3(-2, arrowLeft.position.y, arrowLeft.position.z);
+		arrowRight.position = new Vector3(2, arrowRight.position.y, arrowRight.position.z);
 	}
 
 }
