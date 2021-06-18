1e68042ee76fd777baec9b207f2bdda33751c389
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-06-17 20:08:05 -0400

    refactor menu_arrows to be robust and not hardcoded

--- Assets/Scripts/Menu/MenuArrow.cs
+++ Assets/Scripts/Menu/MenuArrow.cs
@@ -1,29 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuArrow : MonoBehaviour
 {
 
 	[SerializeField] Transform arrowLeft, arrowRight;
 
+	[SerializeField] RectTransform StartButton, HighscoreButton, ExitButton;
+
+	[SerializeField] float arrowSpacing;
+
 	public void MoveToStartButton()
 	{
-		transform.position = new Vector3(transform.position.x, 1.1f, transform.position.z);
-		arrowLeft.position = new Vector3(-5, arrowLeft.position.y, arrowLeft.position.z);
-		arrowRight.position = new Vector3(5, arrowRight.position.y, arrowRight.position.z);
+		MoveToButton(StartButton);
 	}
 
-	public void MoveToHowToPlayButton()
+	public void MoveToHighscoreButton()
 	{
-		transform.position = new Vector3(transform.position.x, -0.7f, transform.position.z);
-		arrowLeft.position = new Vector3(-4, arrowLeft.position.y, arrowLeft.position.z);
-		arrowRight.position = new Vector3(4, arrowRight.position.y, arrowRight.position.z);
+		MoveToButton(HighscoreButton);
 	}
 
 	public void MoveToExitButton()
 	{
-		transform.position = new Vector3(transform.position.x, -2.6f, transform.position.z);
-		arrowLeft.position = new Vector3(-2, arrowLeft.position.y, arrowLeft.position.z);
-		arrowRight.position = new Vector3(2, arrowRight.position.y, arrowRight.position.z);
+		MoveToButton(ExitButton);
+	}
+
+	//Calculates the arrows' positions relative to the button and sets their transform.position to there
+	void MoveToButton(RectTransform Rtransform)
+	{
+		float resMultiple = (float)Screen.width / Screen.currentResolution.width;
+
+		Debug.Log("StartButtonPos: " + StartButton.anchoredPosition);
+
+		Debug.Log("width: " + Screen.currentResolution.width);
+
+		float arrowXL = (Rtransform.anchoredPosition.x - Rtransform.rect.width / 2 - arrowSpacing) * resMultiple;
+		float arrowXR = (Rtransform.anchoredPosition.x + Rtransform.rect.width / 2 + arrowSpacing) * resMultiple;
+		float arrowY = (Rtransform.anchoredPosition.y - Rtransform.rect.height * 0.6f) * resMultiple;
+		Debug.Log("arrowX: " + arrowXL);
+		Debug.Log("arrowY: " + arrowY);
+
+		arrowLeft.position = Camera.main.ScreenToWorldPoint(new Vector3(arrowXL, arrowY, 10));
+		arrowRight.position = Camera.main.ScreenToWorldPoint(new Vector3(arrowXR, arrowY, 10));
 	}
 
 }
