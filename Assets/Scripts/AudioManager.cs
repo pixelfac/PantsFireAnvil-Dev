ffb3bb6dcdddd3fde1e005cf565e78f2c0c0f3ad
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-07-05 15:12:28 -0400

    write AudioManager scripts

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+	void Awake()
+	{
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.volume = s.volume;
+		}
+	}
+
+	public void Play(string name)
+	{
+		AudioSource soundToPlay = null;
+		foreach (Sound s in sounds)
+		{
+			if (s.name == name)
+			{
+				soundToPlay = s.source;
+				break;
+			}
+		}
+		if (soundToPlay == null)
+		{
+			Debug.LogWarning("Sound \"" + name + "\" not found!");
+			return;
+		}
+		
+		soundToPlay.Play();
+
+	}
+
+}
