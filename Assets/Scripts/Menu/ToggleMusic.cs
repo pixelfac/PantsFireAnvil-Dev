a2bc8aa2b859ad231a6a37d23bce4ec3e9c9c10a
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-07-16 23:39:18 -0400

    redirect flow of logic with Toggle Sound buttons to reference the singleton instance of Audiomanager, not the local instance that is deleted

---
+++ Assets/Scripts/Menu/ToggleMusic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleMusic : MonoBehaviour
+{
+	Image musicImage;
+
+	public void Start()
+	{
+		musicImage = gameObject.GetComponent<Image>();
+	}
+
+	public void main(bool val)
+	{
+		if (val)
+		{
+			musicImage.color = new Color(0.5f, 0.5f, 0.5f);
+
+			if (AudioManager.Instance.GetSource("BGM1").isPlaying)
+				AudioManager.Instance.GetSource("BGM1").Pause();
+
+			AudioManager.Instance.GetSource("Victory").volume = 0f;
+			AudioManager.Instance.GetSource("Defeat").volume = 0f;
+		}
+		else
+		{
+			musicImage.color = new Color(1f, 1f, 1f);
+			
+			AudioManager.Instance.GetSource("BGM1").UnPause();
+			AudioManager.Instance.GetSource("Victory").volume = 1f;
+			AudioManager.Instance.GetSource("Defeat").volume = 1f;
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/ToggleSFX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSFX : MonoBehaviour
+{
+	Image sfxImage;
+
+	public void Start()
+	{
+		sfxImage = gameObject.GetComponent<Image>();
+	}
+    public void main(bool val)
+	{
+
+		if (val)
+		{
+			sfxImage.color = new Color(0.5f, 0.5f, 0.5f);
+			AudioManager.Instance.GetSource("Impact").volume = 0f;
+		}
+		else
+		{
+			sfxImage.color = new Color(1f, 1f, 1f);
+			AudioManager.Instance.GetSource("Impact").volume = 1f;
+		}
+	}
+}
